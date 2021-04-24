9120b4096a64f25a17e5fbba16c3d0dd54ed64fc
Sarim <sarim.muqeet2012@gmail.com>
2021-04-23 23:24:28 -0400

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 //route handler with express
 app.get('/',(req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ bye: 'buddy'});
 });
 
 //heroku has the ability to inject environemnt variables to Node's runtime
